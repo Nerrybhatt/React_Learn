84413b55a52c7cc8821fa575a246645127c31e3d
Naresh Bhatta <113630862+Nerrybhatt@users.noreply.github.com>
2025-11-23 08:36:14 +0545

    pricing box is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 // import Fun1 from "./html_jsx.jsx"
-import { Fun1,design,Fun3} from "./html_jsx.jsx"
+import { Fun1,design,Fun3, PricingCard} from "./html_jsx.jsx"
 import Fun2 from "./html_jsx.jsx"
 
 
@@ -11,6 +11,7 @@ function App(){
       <Fun1/>
       <Fun2/>
       <Fun3/>
+      <PricingCard/>
     </div>
   );
 }
