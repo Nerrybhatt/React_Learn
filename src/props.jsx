bb83ad21e880afce7bfa91c4b211342d7c8f9e03
Naresh Bhatta <113630862+Nerrybhatt@users.noreply.github.com>
2025-11-27 22:09:21 +0545

    some beautiful styling is added

--- src/props.jsx
+++ src/props.jsx
@@ -3,18 +3,26 @@ import "./App.css";
 export default function IdCard(props){
     return(
        <div className='card'>
-           <img src={props.imgs} alt="photo" className='photo'/>
-           <h2>{props.name}</h2>
-           <h4>{props.post}</h4>
-            <button className="btn">Message</button>
+           <img src={props.imgs} alt="photo" className='photo'  />
+           <h2 style={{ margin: "5px 0", fontSize: "22px", fontWeight: "700" }}>{props.name}</h2>
+           <h4 style={{ margin: "0", fontSize: "15px", color: "#666" }}>{props.post}</h4>
+            <button className="btn" style={{
+          marginTop: "20px",
+          padding: "12px 25px",
+          background: "#dcd8ff",
+          border: "none",
+          borderRadius: "12px",
+          fontSize: "16px",
+          cursor: "pointer"
+        }}>Message</button>
             <div className="rank">
                 <div className='follow'>
                     <li>followers</li>
-                    <h2>{props.followers}</h2>
+                    <h1>{props.followers}</h1>
                 </div>
                 <div className='follow'>
                     <li>following</li>
-                    <h2>{props.following}</h2>
+                    <h1>{props.following}</h1>
                 </div>
             </div>
        </div>
